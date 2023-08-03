24b92b7817b3440ddc4043a25930562ab30932e8
ArceDanielShok <asd>
2023-08-03 13:27:28 -0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
@@ -9,37 +9,52 @@ const port = 3000;
 const auth0Domain = process.env.AUTH0_DOMAIN;
 const auth0ClientId = process.env.CLIENT_ID;
 const auth0RedirectUri = process.env.AUTH0_REDIRECT_URI;
-const auth0ClientSecret = process.env.CLIENT_SECRET
+const auth0ClientSecret = process.env.CLIENT_SECRET;
 
+// Función para intercambiar el código de autorización por un token de acceso usando la API de Auth0
+const exchangeCodeForToken = async (code) => {
+  const tokenResponse = await axios.post(`https://${auth0Domain}/oauth/token`, {
+    client_id: auth0ClientId,
+    client_secret: auth0ClientSecret,
+    redirect_uri: auth0RedirectUri,
+    code,
+    grant_type: 'authorization_code',
+  });
+  return tokenResponse.data;
+};
 
-// Ruta para obtener la URL de autenticación con Google
+// Función para obtener información del usuario usando el token de acceso
+const getUserInfo = async (accessToken) => {
+  const userInfoResponse = await axios.get(`https://${auth0Domain}/userinfo`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+  return userInfoResponse.data;
+};
+
+// Rutas de autenticación
 app.get('/auth/google', async (req, res) => {
   try {
     const authorizeUrl = `https://${auth0Domain}/authorize?client_id=${auth0ClientId}&redirect_uri=${auth0RedirectUri}&response_type=code&scope=openid profile email&connection=google-oauth2`;
-
-    // Devolver la URL de autenticación al front-end
     res.json({ url: authorizeUrl });
   } catch (error) {
     console.error('Error:', error.message);
     res.status(500).json({ error: 'Error al generar la URL de autenticación' });
   }
 });
-// Ruta para obtener la URL de autenticación con Facebook
+
 app.get('/auth/facebook', async (req, res) => {
   try {
     const authorizeUrl = `https://${auth0Domain}/authorize?client_id=${auth0ClientId}&redirect_uri=${auth0RedirectUri}&response_type=code&scope=openid profile email&connection=facebook`;
-    // Devolver la URL de autenticación al front-end
     res.json({ url: authorizeUrl });
   } catch (error) {
     console.error('Error:', error.message);
     res.status(500).json({ error: 'Error al generar la URL de autenticación' });
   }
 });
+
 app.get('/auth/linkedin', async (req, res) => {
   try {
     const authorizeUrl = `https://${auth0Domain}/authorize?client_id=${auth0ClientId}&redirect_uri=${auth0RedirectUri}&response_type=code&scope=openid%20profile%20email&connection=linkedin`;
-
-    // Devolver la URL de autenticación al front-end
     res.json({ url: authorizeUrl });
   } catch (error) {
     console.error('Error:', error.message);
@@ -47,86 +62,27 @@ app.get('/auth/linkedin', async (req, res) => {
   }
 });
 
-
+// Ruta de callback
 app.get('/auth/callback', async (req, res) => {
   try {
     const code = req.query.code;
+    const tokenData = await exchangeCodeForToken(code);
+    const userInfo = await getUserInfo(tokenData.access_token);
 
-    // Intercambiar el código de autorización por un token de acceso usando la API de Auth0
-    const tokenResponse = await fetch(`https://${auth0Domain}/oauth/token`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        client_id: auth0ClientId,
-        client_secret: auth0ClientSecret,
-        redirect_uri: auth0RedirectUri,
-        code,
-        grant_type: 'authorization_code',
-      }),
-    });
-    const tokenData = await tokenResponse.json();
-
-    // Usar el token de acceso para obtener información del usuario
-    const userInfoResponse = await fetch(`https://${auth0Domain}/userinfo`, {
-      headers: { Authorization: `Bearer ${tokenData.access_token}` },
-    });
-    const userInfo = await userInfoResponse.json();
-
-    return res.json({
+    res.json({
       profile: userInfo,
-      access_token: "access_token",
-      refresh_token: "refresh_token"
-    })
-
+      access_token: tokenData.access_token,
+      refresh_token: tokenData.refresh_token,
+    });
   } catch (error) {
     console.error('Error:', error.message);
-    res.status(500).json({ error: 'Error al generar la URL de autenticación' });
-  }
-});
-app.get('/', async (req, res) => {
-  try {
-    res.json({ url: "hello world!" });
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Error al generar la URL de autenticación' });
+    res.status(500).json({ error: 'Error al procesar la autorización' });
   }
 });
 
-
-app.get('/auth/callback', async (req, res) => {
-  try {
-    const code = req.query.code;
-
-    // Intercambiar el código de autorización por un token de acceso usando la API de Auth0
-    const tokenResponse = await fetch(`https://${auth0Domain}/oauth/token`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        client_id: auth0ClientId,
-        client_secret: auth0ClientSecret,
-        redirect_uri: auth0RedirectUri,
-        code,
-        grant_type: 'authorization_code',
-      }),
-    });
-    const tokenData = await tokenResponse.json();
-
-    // Usar el token de acceso para obtener información del usuario
-    const userInfoResponse = await fetch(`https://${auth0Domain}/userinfo`, {
-      headers: { Authorization: `Bearer ${tokenData.access_token}` },
-    });
-    const userInfo = await userInfoResponse.json();
-
-    return res.json({
-      profile: userInfo,
-      access_token: "access_token",
-      refresh_token: "refresh_token"
-    })
-
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Error al generar la URL de autenticación' });
-  }
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello world!' });
 });
 
 // Iniciar el servidor
